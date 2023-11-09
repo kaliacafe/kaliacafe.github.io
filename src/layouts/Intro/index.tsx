d9b958bb8cdf188da00fd586dfb00dfbccbd05f1
Eric <eric@basiltech.org>
2023-11-09 10:02:19 -1000

    Add more framework for project

---
+++ src/layouts/Intro/index.tsx
@@ -0,0 +1,19 @@
+const Intro = () => (
+	<div id="intro">
+		<h1>Kalia Cafe</h1>
+		<h2>Sandwich, Espresso, Internet Cafe</h2>
+		<h2>Kihei, Maui, Hawaii</h2>
+		<p>
+			Formerly Cafe @ la Plage<br />
+			6:45am-1:30pm<br />
+			Closed Tuesdays
+		</p>
+		<ul className="actions">
+			<li>
+				<a href="#header" className="button icon solid solo fa-arrow-down scrolly">Continue</a>
+			</li>
+		</ul>
+	</div>
+)
+
+export default Intro;
